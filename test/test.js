1a0866a5944748f76c073f1a4d487e70c5e513b1
Your Name <you@example.com>
2018-04-26 21:14:09 -0700

    blah

--- test/test.js
+++ test/test.js
@@ -1,10 +1,40 @@
 
-const http = require('./http');
+//const http = require('./http');
+//
+//(async () => {
+//	
+//
+//	
+//})().catch(err => console.log(err))
 
-(async () => {
-	
-	const res = await http.doGET('http://www.google.com')
-	
-	console.log(res)
-	
-})().catch(err => console.log(err))
+const { Client } = require('tplink-smarthome-api');
+
+const client = new Client();
+
+//const plug = client.getDevice({host: '10.0.1.2'}).then((device)=>{
+//  device.getSysInfo().then(console.log);
+//  device.setPowerState(true);
+//});
+
+// Look for devices, log to console, and turn them on
+//client.startDiscovery().on('device-new', (device) => {
+//
+//	console.log(device.emter)
+//  
+//});
+
+//client.getPlug({host: '192.168.1.118'}).then((plug) => {
+//	console.log(plug)
+//});
+
+//console.log(
+//		client.getPlug({host: '192.168.1.118'}).emeter.get_realtime
+//		)
+
+console.log(
+
+		client.getPlug({host: '192.168.1.118'}).emeter.getRealtime().then(blah => {
+			console.log(blah)
+		})
+		
+)
